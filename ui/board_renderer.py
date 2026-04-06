7a7a1147ee8de609d456d55036d9fee09077d0e9
FraughtCoin <FraughtCoin@yahoo.com>
2026-04-07 01:07:12 +0300

    Added click detection and line highlighting

--- ui/board_renderer.py
+++ ui/board_renderer.py
@@ -1,5 +1,5 @@
 import pygame
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class BoardRenderer:
@@ -15,14 +15,17 @@ class BoardRenderer:
         self.bar_width = bar_width
         self.colors = colors
     
-    def draw_board(self):
+    def draw_board(self, selected_line: Optional[int]):
         """
         Draw the complete board.
+        Args:
+            selected_line: the line number to highlight or None
         """
         self._draw_background()
         self._draw_points()
         self._draw_bar()
         self._draw_border()
+        self._draw_highlight(selected_line)
     
     def _draw_background(self):
         """
@@ -84,9 +87,17 @@ class BoardRenderer:
                 point_index
             )
     
-    def _draw_triangle_down(self, x, y, width, height, color, point_num):
+    def _draw_triangle_down(self, x: int, y: int, width: int, height: int,
+                            color: Tuple[int, int, int], point_num: int):
         """
         Draw downward pointing triangle.
+        Args:
+            x: x-coordinate of top-left corner
+            y: y-coordinate of top-left corner
+            width: width of triangle base
+            height: height of triangle
+            color: RGB color tuple
+            point_num: point number to display
         """
         points = [
             (x, y),
@@ -101,9 +112,17 @@ class BoardRenderer:
         text_rect = text.get_rect(center=(x + width // 2, y + 15))
         self.screen.blit(text, text_rect)
     
-    def _draw_triangle_up(self, x, y, width, height, color, point_num):
+    def _draw_triangle_up(self, x: int, y: int, width: int, height: int,
+                            color: Tuple[int, int, int], point_num: int):
         """
         Draw upward pointing triangle.
+        Args:
+            x: x-coordinate of top-left corner
+            y: y-coordinate of top-left corner
+            width: width of triangle base
+            height: height of triangle
+            color: RGB color tuple
+            point_num: point number to display
         """
         points = [
             (x, y),
@@ -129,3 +148,70 @@ class BoardRenderer:
                         (bar_x, bar_y, self.bar_width, self.board_height))
         pygame.draw.rect(self.screen, self.colors['black'],
                         (bar_x, bar_y, self.bar_width, self.board_height), 2)
+        
+    def _get_line_position(self, line) -> Tuple[int, int, bool]:
+        """
+        Get the center position of a line.
+        Args:
+            line: the line number (0-23)
+        Returns:
+            tuple of (x, y, is_top) where is_top indicates if line is on top
+            half
+        """
+        if line > 11:
+            is_top = True
+            visual_index = line - 12
+            x_offset = visual_index * self.point_width
+            
+            if visual_index >= 6:
+                x_offset += self.bar_width
+            
+            x = self.board_x + x_offset + self.point_width // 2
+            y = self.board_y
+        else:
+            is_top = False
+            visual_index = 11 - line
+            x_offset = visual_index * self.point_width
+            
+            if visual_index >= 6:
+                x_offset += self.bar_width
+            
+            x = self.board_x + x_offset + self.point_width // 2
+            y = self.board_y + self.board_height
+        
+        return x, y, is_top
+        
+    def _draw_highlight(self, selected_line: Optional[int]):
+        """
+        Draw highlight on selected point.
+        Args:
+            selected_line: the line number to highlight (0-23), or None
+        """
+        if selected_line is None:
+            return
+        
+        line_x, line_y, is_top = self._get_line_position(selected_line)
+
+        highlight_surface = pygame.Surface((self.point_width,
+                                            self.point_height),
+                                            pygame.SRCALPHA)
+        highlight_surface.fill(self.colors['highlight'])
+
+        if is_top:
+            visual_index = selected_line - 12
+            x_offset = visual_index * self.point_width
+            if visual_index >= 6:
+                x_offset += self.bar_width
+
+            x = self.board_x + x_offset
+            y = self.board_y
+        else:
+            visual_index = 11 - selected_line
+            x_offset = visual_index * self.point_width
+            if visual_index >= 6:
+                x_offset += self.bar_width
+
+            x = self.board_x + x_offset
+            y = self.board_y + self.board_height - self.point_height
+
+        self.screen.blit(highlight_surface, (x, y))
--- ui/pygame_ui.py
+++ ui/pygame_ui.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from typing import Tuple
+from typing import Tuple, Optional
 from game import BackgammonGame
 from .board_renderer import BoardRenderer
 from .token_renderer import TokenRenderer
@@ -18,7 +18,7 @@ class PygameUI:
     BOARD_HEIGHT = 700
 
     LINE_WIDTH = 80
-    LINE_HEIGHT = 250
+    LINE_HEIGHT = 320
     BAR_WIDTH = 60
 
     TOKEN_RADIUS = 30
@@ -35,6 +35,7 @@ class PygameUI:
         'board_background': (255, 204, 153),
         'point_light': (153, 204, 153),
         'point_dark': (102, 153, 102),
+        'highlight': (255, 255, 0, 128),
     }
 
     def __init__(self) -> None:
@@ -53,6 +54,8 @@ class PygameUI:
 
         self.board_x = self.BOARD_MARGIN
         self.board_y = self.BOARD_MARGIN
+
+        self.selected_line = None
 
         self.board_renderer = BoardRenderer(
             self.screen, self.board_x, self.board_y,
@@ -80,13 +83,66 @@ class PygameUI:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self.handle_click(event.pos)
+
+    def handle_click(self, pos: Tuple[int, int]):
+        """
+        Handle mouse click events on the board
+        Args:
+            pos - tuple of (x, y) coordinates of the click
+        """
+        clicked_line = self.get_line_at_position(pos)
+        if clicked_line is not None:
+            if self.selected_line == clicked_line:
+                self.selected_line = None
+            else:
+                self.selected_line = clicked_line
+        else:
+            self.selected_line = None
+
+    def get_line_at_position(self, pos: Tuple[int, int]) -> Optional[int]:
+        """
+        Determine which line on the board was clicked
+        Args:
+            pos - tuple of (x, y) coordinates of the click
+        Returns:
+            line number (0-23) or None if click is outside board
+        """
+        x, y = pos
+        if not (self.board_x <= x <= self.board_x + self.BOARD_WIDTH 
+                 and self.board_y <= y <= self.board_y + self.BOARD_HEIGHT):
+            return None
+        
+        rel_x = x - self.board_x
+        rel_y = y - self.board_y
+        is_top = rel_y < self.BOARD_HEIGHT / 2
+
+        bar_start = 6 * self.LINE_WIDTH
+        bar_end = bar_start + self.BAR_WIDTH
+        if bar_start <= rel_x <= bar_end:
+            return None
+
+        if rel_x > bar_start:
+            rel_x -= self.BAR_WIDTH
+
+        column = int(rel_x / self.LINE_WIDTH)
+        if column < 0 or column >= 12:
+            return None
+
+        if is_top:
+            return 12 + column
+        else:
+            return 11 - column
+
 
     def update(self):
         pass
 
     def draw(self):
         self.screen.fill(self.COLORS['background'])
-        self.board_renderer.draw_board()
+        self.board_renderer.draw_board(self.selected_line)
         self.token_renderer.draw_all_tokens(self.game.get_board())
         pygame.display.flip()
 
